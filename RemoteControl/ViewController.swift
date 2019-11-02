3b8947b8c500d99021f573d8fd22ac0d2e843241
Venj <ersaclarke@gmail.com>
2019-11-02 12:56:49 +0800

    Initial Commit

---
+++ RemoteControl/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RemoteControl
+//
+//  Created by 朱文杰 on 11/2/19.
+//  Copyright © 2019 Sukiapps. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
