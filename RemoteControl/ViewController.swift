6ac81c28566e06d8588514001ea7be5a62f31903
Venj <ersaclarke@gmail.com>
2019-11-08 16:02:23 +0800

    Remote control.

--- RemoteControl/ViewController.swift
+++ RemoteControl/ViewController.swift
@@ -10,11 +10,31 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var ipField: UITextField!
+    @IBOutlet var switchFields: [UISwitch]!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func valueChanged(_ sender: UISwitch) {
+        let tag = sender.tag
+        turn(sender.isOn, switchIndex: tag)
+    }
 
+    func turn(_ on: Bool, switchIndex index: Int) {
+        guard let ip = ipField?.text else { return }
+        let operation = on ? "on" : "off"
+        let urlString = "http://\(ip)/relay?op=\(operation)&pin=\(index)"
+        guard let url = URL(string: urlString) else { return }
+        _ = try? Data(contentsOf: url, options: [])
+    }
 }
 
+extension ViewController: UITextFieldDelegate {
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        ipField.resignFirstResponder()
+        return true
+    }
+}
